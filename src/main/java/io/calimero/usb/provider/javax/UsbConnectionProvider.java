9a4248933e008537885bb0b09f7a0d2a2b21c480
bmalinowsky <bmalinowsky@github.com>
2024-03-11 16:43:14 +0100

    Override toString

--- src/main/java/io/calimero/usb/provider/javax/UsbConnectionProvider.java
+++ src/main/java/io/calimero/usb/provider/javax/UsbConnectionProvider.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2022, 2023 B. Malinowsky
+    Copyright (c) 2022, 2024 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -62,4 +62,7 @@ public final class UsbConnectionProvider implements io.calimero.serial.usb.spi.U
 			return devices.collect(Collectors.toUnmodifiableSet());
 		}
 	}
+
+	@Override
+	public String toString() { return "usb4java connection provider"; }
 }
